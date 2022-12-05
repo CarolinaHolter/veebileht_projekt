0cae13f0c404678053c8a25f5a6c7ce7f9998101
merili2003 <117356409+merili2003@users.noreply.github.com>
2022-12-06 01:14:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-// Select all the elements in the HTML page
-// and assign them to a variable
+//Javascripti kood on kopeeritud järgmiselt lehelt: https://www.geeksforgeeks.org/create-a-music-player-using-javascript/
+//HTML-lehe elementide määramine muutujatele
 let now_playing = document.querySelector(".now-playing");
 let track_art = document.querySelector(".track-art");
 let track_name = document.querySelector(".track-name");
@@ -14,15 +14,15 @@ let volume_slider = document.querySelector(".volume_slider");
 let curr_time = document.querySelector(".current-time");
 let total_duration = document.querySelector(".total-duration");
 
-// Specify globally used values
+// Globaalsete kasutatavate väärtuste määramine
 let track_index = 0;
 let isPlaying = false;
 let updateTimer;
 
-// Create the audio element for the player
+// Audio element playeri jaoks
 let curr_track = document.createElement('audio');
 
-// Define the list of tracks that have to be played
+// Esitatavate lugude loend
 let track_list = [
 {
 	name: "Last Christmas",
@@ -57,15 +57,15 @@ let track_list = [
 ];
 
 function loadTrack(track_index) {
-    // Clear the previous seek timer
+    // Eelmise timeri tühjendamine
     clearInterval(updateTimer);
     resetValues();
     
-    // Load a new track
+    // Uue tracki laadimine
     curr_track.src = track_list[track_index].path;
     curr_track.load();
     
-    // Update details of the track
+    // Tracki detailide värskendamine
     track_art.style.backgroundImage =
         "url(" + track_list[track_index].image + ")";
     track_name.textContent = track_list[track_index].name;
@@ -73,33 +73,31 @@ function loadTrack(track_index) {
     now_playing.textContent =
         "PLAYING " + (track_index + 1) + " OF " + track_list.length;
     
-    // Set an interval of 1000 milliseconds
-    // for updating the seek slider
+    // 1000-millisekundiline intervall slideri värskendamiseks
     updateTimer = setInterval(seekUpdate, 1000);
     
-    // Move to the next track if the current finishes playing
-    // using the 'ended' event
+    // Järgmise tracki juurde liikumine, kui praegune lõpetab mängimise 
+    // kasutades 'ended' eventi
     curr_track.addEventListener("ended", nextTrack);
     
-    // Apply a random background color
+    // Soovipärase taustavärvi valimine
     random_bg_color();
     }
     
     function random_bg_color() {
-    // Get a random number between 64 to 256
-    // (for getting lighter colors)
+    // Arv vahemikus 64-256 annab heledama värvi
     let red = Math.floor(Math.random() * 256) + 64;
     let green = Math.floor(Math.random() * 256) + 64;
     let blue = Math.floor(Math.random() * 256) + 64;
     
-    // Construct a color with the given values
+    // Antud väärtustega värvi kontrueerimine
     let bgColor = "rgb(" + red + ", " + green + ", " + blue + ")";
     
-    // Set the background to the new color
+    // Uue värvi seadmine taustale
     document.body.style.background = bgColor;
     }
     
-    // Function to reset all values to their default
+    // Funktsioon kõigi väärtuste vaikeväärtusele lähtestamiseks
     function resetValues() {
     curr_time.textContent = "00:00";
     total_duration.textContent = "00:00";
@@ -107,94 +105,88 @@ function loadTrack(track_index) {
     }
 
     function playpauseTrack() {
-        // Switch between playing and pausing
-        // depending on the current state
+        // Play ja pause vaheldumisi aktiveerimine sõltuvalt praegusest olekust
         if (!isPlaying) playTrack();
         else pauseTrack();
         }
         
         function playTrack() {
-        // Play the loaded track
+        // Loaded tracki mängimine 
         curr_track.play();
         isPlaying = true;
         
-        // Replace icon with the pause icon
+        // Asendamine pausiikooniga 
         playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
         }
         
         function pauseTrack() {
-        // Pause the loaded track
+        // Loaded tracki peatamine
         curr_track.pause();
         isPlaying = false;
         
-        // Replace icon with the play icon
+        // Asendamine play-ikooniga
         playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
         }
         
         function nextTrack() {
-        // Go back to the first track if the
-        // current one is the last in the track list
+        // Esimese loo juurde naasmine (praegune on loendis viimane)
         if (track_index < track_list.length - 1)
             track_index += 1;
         else track_index = 0;
         
-        // Load and play the new track
+        // Uue loo laadimine ja esitamine
         loadTrack(track_index);
         playTrack();
         }
         
         function prevTrack() {
-        // Go back to the last track if the
-        // current one is the first in the track list
+        // Viimase loo juurde naasmine (praegune on loendis esimene)
         if (track_index > 0)
             track_index -= 1;
         else track_index = track_list.length - 1;
         
-        // Load and play the new track
+        // Uue loo laadimine ja esitamine
         loadTrack(track_index);
         playTrack();
         }
         function seekTo() {
-            // Calculate the seek position by the
-            // percentage of the seek slider
-            // and get the relative duration to the track
+            //Seek positioni arvutamine slideri protsendi ja tracki kestvuse järgi
             seekto = curr_track.duration * (seek_slider.value / 100);
             
-            // Set the current track position to the calculated seek position
+            //  Praeguse tarcki asukoha seadmine arvutatud seek positionile
             curr_track.currentTime = seekto;
             }
             
             function setVolume() {
-            // Set the volume according to the
-            // percentage of the volume slider set
+            // Helitugevuse seadmine vastavalt helitugevuse slideri protsendile
             curr_track.volume = volume_slider.value / 100;
             }
             
             function seekUpdate() {
             let seekPosition = 0;
             
-            // Check if the current track duration is a legible number
+            // Kontroll, kas praegune tracki kestus on loetav number
             if (!isNaN(curr_track.duration)) {
                 seekPosition = curr_track.currentTime * (100 / curr_track.duration);
                 seek_slider.value = seekPosition;
             
-                // Calculate the time left and the total duration
+                // Järelejäänud aja ja kogukestuse arvutamine
                 let currentMinutes = Math.floor(curr_track.currentTime / 60);
                 let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
                 let durationMinutes = Math.floor(curr_track.duration / 60);
                 let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
             
-                // Add a zero to the single digit time values
+                // Ühekohalistele ajaväärtustele nulli lisamine
                 if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
                 if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
                 if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
                 if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
             
-                // Display the updated duration
+                // Värskendatud kestuse kuvamine
                 curr_time.textContent = currentMinutes + ":" + currentSeconds;
                 total_duration.textContent = durationMinutes + ":" + durationSeconds;
             }
             }
-// Load the first track in the tracklist
+// Esimese loo laadimine tracklisti
 loadTrack(track_index);
                     
